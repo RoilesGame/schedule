490ebdf9e1de05cdadfcfdda4e26acd086174199
MrEndor <roilesgame0799@gmail.com>
2023-04-30 00:16:46 +0300

    Добавил модели юзеров

---
+++ server/persistence/users/models.py
@@ -0,0 +1,46 @@
+import datetime
+
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from server.db import db
+
+
+class User(db.Model, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+    name = sqlalchemy.Column(
+        sqlalchemy.String,
+        nullable=True,
+    )
+    about = sqlalchemy.Column(
+        sqlalchemy.String,
+        nullable=True,
+    )
+    email = sqlalchemy.Column(
+        sqlalchemy.String,
+        index=True,
+        unique=True,
+        nullable=True,
+    )
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(
+        sqlalchemy.DateTime,
+        default=datetime.datetime.now,
+    )
+
+    def __repr__(self):
+        return f'<User> {self.id} {self.name} {self.email}'
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
